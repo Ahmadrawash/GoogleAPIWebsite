1feb812ed71a9d96c5ff2027a40fc5e5dc9caab9
ahmadrawash <ahmedyr2003@gmail.com>
2024-04-07 17:32:59 +0300

    Add project files.

---
+++ GoogleAPIWebsite/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GoogleAPIWebsite._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Google API Homework </h1><h6>Dr. Ahmad Rawashdeh's class</h6>
+    <header>
+        <script type="text/javascript" >           
+        </script>    
+    </header>
+    <main>
+        <br />
+        input any query, the cx of your program search, and the key of your google developers account, then click on search and save the returned file. 
+        <br />
+        <br />
+        <div class="row">
+            <section class="col-md-4" aria-labelledby="gettingStartedTitle">
+                <asp:Label runat="server" Text="Input Query" ID="Label1">Input Query</asp:Label>
+                <asp:TextBox runat="server" ID="queryTextBox"></asp:TextBox>
+            </section>
+            <section class="col-md-4" style="align-content:center" aria-labelledby="gettingStartedTitle">
+                <asp:Label runat="server" Text="Input cx" ID="Label4">Input cx</asp:Label>
+                <asp:TextBox runat="server" ID="cxTextBox"></asp:TextBox>
+                <br />
+            </section>
+            <section class="col-md-4" aria-labelledby="gettingStartedTitle">
+                 <asp:Label runat="server" Text="Input key" ID="Label3">Input key</asp:Label>
+                <asp:TextBox runat="server" ID="keyTextBox"></asp:TextBox>
+                <br />
+            </section>
+        </div>
+        <asp:Button runat="server" Text="Search" style="align-content:center" onClientClick="search();" onclick="Unnamed2_Click"></asp:Button>
+              
+        <div style="align-content:center" >
+            <asp:Label runat="server" ID="outputLabel">...</asp:Label>
+        </div>
+
+    </main>
+
+</asp:Content>
---
+++ GoogleAPIWebsite/Default.aspx.cs
@@ -0,0 +1,104 @@
+﻿/*
+ * author: Dr. Ahmad Rawashdeh
+ * course: Web Technology BIT 386
+ * student name: 
+ * student number:
+ */
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GoogleAPIWebsite
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed2_Click(object sender, EventArgs e)
+        {
+            
+            String key = "";
+            String query = ""; 
+            String cx = "";
+            String results = "";
+
+           
+            //get the text of the textboxes (query, cx, and key)
+            query = queryTextBox.Text.Trim();
+            cx = cxTextBox.Text.Trim();
+            key = keyTextBox.Text.Trim();
+
+          
+            //error checking
+            if (query == null || query.Equals(""))
+            {
+                outputLabel.Text = "error: query is empty";
+                return;
+            }
+            if (key == null || key.Equals(""))
+            {
+                outputLabel.Text = "error: search key is empty or not found. you should create a txt file \"key.txt\" in the debug folder of your project";
+                return;
+            }
+            if (cx == null || cx.Equals(""))
+            {
+                outputLabel.Text = "error: search program id wasn't found. you should create a text file \"searchProgram.txt\" in the debug folder of your project";
+                return;
+            }
+
+            
+            //start using the Goolge API search
+            try
+            {
+                //create the request object
+                //GET https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+                HttpWebRequest hwr = (HttpWebRequest)HttpWebRequest.Create("https://www.googleapis.com/customsearch/v1?key=" + key + "&cx=" + cx + "&q=" + query);
+                hwr.Method = "GET";
+
+                //send the request and get the response (result) object. This is where the Google API Web service is used
+                HttpWebResponse wr = (HttpWebResponse)hwr.GetResponse();
+                Stream stre = wr.GetResponseStream();
+                StreamReader responseSr = new StreamReader(stre);
+                results =  responseSr.ReadToEnd();
+                
+                //create a file of type json which has as a content the response returned from google search web service above
+                StreamWriter sw = new StreamWriter(Server.MapPath("result_" + query + ".json"));
+                sw.Write(results);
+                sw.Close();
+
+                //display the response of the google web service as the text of the label
+                outputLabel.Text = results;
+
+                //send the json file to client for download
+                Response.Clear();
+                Response.Buffer = true;
+                Response.Charset = "";
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.ContentType = "application/json";
+                Response.AddHeader("content-disposition", "attachment; filename=result_" + query + ".json");
+                Response.WriteFile(Server.MapPath("~/result_" + query + ".json"));
+                Response.Flush();
+                Response.End();                          
+                return;
+            }
+
+            catch (Exception exc)
+            {
+                outputLabel.Text = "Exeption: " + exc.Message + "\n" + exc.StackTrace;
+            }
+
+        }
+
+    }
+}
